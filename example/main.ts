a1ea7cce7591626632ca04f2285d97f53dc5537d
HGGshiwo <3190102788@zju.edu.cn>
2024-06-26 21:50:49 +0800

    update

--- example/main.ts
+++ example/main.ts
@@ -1,7 +1,6 @@
-import 'jspsych/css/jspsych.css';
-import '../src/index.css'
-import { createApp } from "vue";
-import App from "./App.vue";
+import 'jspsych/css/jspsych.css'
+import { createApp } from 'vue'
+import App from './App.vue'
 
 const app = createApp(App)
-app.mount("#app")
+app.mount('#app')
